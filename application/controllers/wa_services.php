6339db22cce5c74ba099f843c319698b5ee93502
Bayu Yuhartono <bayuyuhartono@gmail.com>
2023-04-11 16:24:06 +0700

    curl

--- application/controllers/wa_services.php
+++ application/controllers/wa_services.php
@@ -8,6 +8,16 @@ class wa_services extends REST_Controller
 		$this->checkToken();
     }
 
+    public function generateToken()
+    {
+        $postData["username"] = "use";
+        $postData["password"] = "pas";
+
+        $res = $this->curlPostRequest('users/login',$postData);
+
+        return $res;
+    }
+
     public function single()
     {
         $error = [];
@@ -26,9 +36,58 @@ class wa_services extends REST_Controller
             ], 400);
         }
 
+        $postData["to"] = "628123456789";
+        $postData["recipient_type"] = "individual";
+        $postData["type"] = "text";
+        $postData["text"]['body'] = "some text to be sent";
+        $postData["preview_url"] = false;
+
+        $res = $this->curlPostRequest('messages',$postData);
+
         $this->response([
             'success' => true,
             'message' => 'sending message success',
+            'data' => $res,
         ], 200);
+    }
+
+    public function curlPostRequest($endPoint, $postData)
+    {
+        $watoken = 'mwemfooepfwpfpofmo22fmp2o3f2pfmofpo3fmpp23kdfpok3';
+        $baseUrl = 'https://api-whatsapp.kata.ai/v1';
+        $url = $baseUrl.'/'.$endPoint;
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($postData));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/x-www-form-urlencoded',
+            'Authorization: '.$watoken,
+        ));
+        $result = curl_exec($curl);
+        curl_close($curl);
+        $result = json_decode($result, true);
+        return $result;
+    }
+
+    public function curlPostFormDataRequest($endPoint, $postData)
+    {
+        $watoken = 'mwemfooepfwpfpofmo22fmp2o3f2pfmofpo3fmpp23kdfpok3';
+        $baseUrl = 'https://api-whatsapp.kata.ai/v1';
+        $url = $baseUrl.'/'.$endPoint;
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($postData));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: multipart/form-data',
+            'Authorization: '.$watoken,
+        ));
+        $result = curl_exec($curl);
+        curl_close($curl);
+        $result = json_decode($result, true);
+        return $result;
     }
 }
